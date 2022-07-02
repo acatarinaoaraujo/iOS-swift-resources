626c5fbb13093d86a061282de418c08a783e4bc1
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-01 16:14:21 -1000

    Initial

--- study_guide.swift
+++ study_guide.swift
@@ -8,10 +8,19 @@
 
 /* 3. Random */
     Int.random(in: 1 ... 3)
-    Float.random(in: 1 ... < 3) // [not including upper bound]
+    Float.random(in: 1 ... < 3)         // [not including upper bound]
 
     let array = [1, 2, 3]
     array.randomElement()
     array.shuffle()                     // [items change position in the array]
 
 // 4. Functions and Scope
+    func greeting1() {
+        print("Hello")
+
+        func greeting2() {
+            print("Hey")
+        }
+        greeting2()                     // [calling outside of greeting1 does not work]
+    }
+
