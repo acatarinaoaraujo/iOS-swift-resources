7b1d8264ecb7bb1a6ff8418c600f037fb6055cf0
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-01 16:09:13 -1000

    Initial

---
+++ study_guide.swift
@@ -0,0 +1,17 @@
+/* 1. Naming Conventions
+    - camelCase: application programs
+    - kebab-case: web development
+    - snake_case: file naming */
+
+/* 2. Print Statament */
+    print("Hello \(2+3) World")
+
+/* 3. Random */
+    Int.random(in: 1 ... 3)
+    Float.random(in: 1 ... < 3) // [not including upper bound]
+
+    let array = [1, 2, 3]
+    array.randomElement()
+    array.shuffle()                     // [items change position in the array]
+
+// 4. Functions and Scope
