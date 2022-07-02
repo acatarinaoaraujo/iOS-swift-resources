6a56d0c039011572ac59596b14c896809d2645c2
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-01 18:04:56 -1000

    update

--- study_guide.swift
+++ study_guide.swift
@@ -1,4 +1,5 @@
 /* 1. Naming Conventions
+
     - camelCase: application programs
     - kebab-case: web development
     - snake_case: file naming */
@@ -7,6 +8,7 @@
     print("Hello \(2+3) World")
 
 /* 3. Random */
+
     Int.random(in: 1 ... 3)
     Float.random(in: 1 ... < 3)         // [not including upper bound]
 
@@ -15,6 +17,7 @@
     array.shuffle()                     // [items change position in the array]
 
 // 4. Functions and Scope
+
     func greeting1() {
         print("Hello")
 
@@ -24,3 +27,64 @@
         greeting2()                     // [calling outside of greeting1 does not work]
     }
 
+// 5. Type Inteference
+
+    var cookies = 4                     // [able to infer the datatype]
+
+// 6. Dictionaries
+
+    var dict : [String: Int] = ["Angela" : 4, "John" : 7]
+
+// 7. Unwrapping Optionals
+
+    var player1: String? = nil
+    player1 = "John Smith"
+    print(player1!)                     // [became a normal string]
+
+    player1 = nil
+    print(player1!)                     // [error: can't unwrap nil value]
+
+// 8. Struct = Blueprint
+
+    struct Town {
+        /* properties */
+        let name = "Disneyland"
+        var citizens = ["Catariana", "Louis"]
+
+        /* method */
+        func fortify() {
+            print("Defenses increased!")
+        }
+    }
+
+    var myTown = Town()
+    myTown.citizens
+    myTown.citizens.append("Richard")
+
+    // Why we have initializers? Be able to customize each town.
+
+    struct Town {
+        let name: String                // [immutable]
+        var citizens: [String]          // [mutable]
+
+        init(name: String, citizens: [String]) {
+            self.name = name
+            self.citizens = citizens
+        }
+
+        func fortify() {
+            print("Defenses increased!")
+        }
+    }
+
+    var town = Town(townName: "Munich", people: ["Tom Hanks"])
+    town.citizens
+
+// 9. Immutability
+    /* In order to modify any properties within the struct, 'mutating' must be added. */
+    struct Town {
+        // [...]
+        mutating func harvestRice() {
+            citizens.append("Jason")
+        }
+    }
